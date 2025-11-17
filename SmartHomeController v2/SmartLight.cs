7b90de1baa2a8e33d4b48b8cc7c4a419b6b14607
b00970775 <beggs-g4@ulster.ac.uk>
2025-11-17 15:03:40 +0000

    added superclass and new methods

--- SmartHomeController v2/SmartLight.cs
+++ SmartHomeController v2/SmartLight.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SmartHomeController
 {
-    public class SmartLight
+    public class SmartLight : SmartDevice
     {
         // Private fields
         private double brightness;
@@ -26,7 +26,11 @@ namespace SmartHomeController
 
         // Constructor
         
-
+        public SmartLight (int deviceID, string deviceName, double brightness, string colour) : base (deviceID, deviceName)
+        {
+            this.brightness = brightness;
+            this.colour = colour;
+        }
 
         public void SetBrightness(double brightness)
         {
@@ -38,6 +42,12 @@ namespace SmartHomeController
         {
             this.Colour = colour;
             Console.WriteLine($"Colour now set to {Colour}");
+        }
+
+        public override void GetStatus()
+        {
+            base.GetStatus();
+            Console.WriteLine($"Brightness: {Brightness}%, Colour: {colour}");
         }
        
     }
